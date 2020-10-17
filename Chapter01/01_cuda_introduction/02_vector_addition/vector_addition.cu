0cbc8106603f9ac1b73478c462f421748474e361
feitang0 <iamtangfei@gmail.com>
2020-10-17 13:44:30 +0800

    reformat the code

--- Chapter01/01_cuda_introduction/02_vector_addition/vector_addition.cu
+++ Chapter01/01_cuda_introduction/02_vector_addition/vector_addition.cu
@@ -1,38 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define N 512
 
 void host_add(int *a, int *b, int *c) {
-	for(int idx=0;idx<N;idx++)
-		c[idx] = a[idx] + b[idx];
+  for (int idx = 0; idx < N; idx++)
+    c[idx] = a[idx] + b[idx];
 }
 
-//basically just fills the array with index.
+// basically just fills the array with index.
 void fill_array(int *data) {
-	for(int idx=0;idx<N;idx++)
-		data[idx] = idx;
+  for (int idx = 0; idx < N; idx++)
+    data[idx] = idx;
 }
 
-void print_output(int *a, int *b, int*c) {
-	for(int idx=0;idx<N;idx++)
-		printf("\n %d + %d  = %d",  a[idx] , b[idx], c[idx]);
+void print_output(int *a, int *b, int *c) {
+  for (int idx = 0; idx < N; idx++)
+    printf("\n %d + %d  = %d", a[idx], b[idx], c[idx]);
 }
 int main(void) {
-	int *a, *b, *c;
-	int size = N * sizeof(int);
+  int *a, *b, *c;
+  int size = N * sizeof(int);
 
-	// Alloc space for host copies of a, b, c and setup input values
-	a = (int *)malloc(size); fill_array(a);
-	b = (int *)malloc(size); fill_array(b);
-	c = (int *)malloc(size);
+  // Alloc space for host copies of a, b, c and setup input values
+  a = (int *)malloc(size);
+  fill_array(a);
+  b = (int *)malloc(size);
+  fill_array(b);
+  c = (int *)malloc(size);
 
-	host_add(a,b,c);
+  host_add(a, b, c);
 
-	print_output(a,b,c);
+  print_output(a, b, c);
 
-	free(a); free(b); free(c);
+  free(a);
+  free(b);
+  free(c);
 
-
-	return 0;
+  return 0;
 }
