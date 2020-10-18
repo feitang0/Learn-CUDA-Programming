dc96c94e73084938f6b40bcdfa8457eab5dca21a
feitang0 <iamtangfei@gmail.com>
2020-10-18 20:18:55 +0800

    reformat the code

--- Chapter02/02_memory_overview/03_aos_soa/soa.cu
+++ Chapter02/02_memory_overview/03_aos_soa/soa.cu
@@ -1,8 +1,7 @@
-#include<stdio.h>
-#include<iostream>
-#include<stdlib.h>
-#include<string.h>
-
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define NUM_THREADS 256
 
@@ -10,45 +9,41 @@
 
 // Coefficients with Structure of Array
 struct Coefficients_SOA {
-  int* r;
-  int* b;
-  int* g;
-  int* hue;
-  int* saturation;
-  int* maxVal;
-  int* minVal;
-  int* finalVal; 
+  int *r;
+  int *b;
+  int *g;
+  int *hue;
+  int *saturation;
+  int *maxVal;
+  int *minVal;
+  int *finalVal;
 };
 
-
-__global__
-void complicatedCalculation(Coefficients_SOA  data)
-{
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  int grayscale = (data.r[i] + data.g[i] + data.b[i])/data.maxVal[i];
+__global__ void complicatedCalculation(Coefficients_SOA data) {
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  int grayscale = (data.r[i] + data.g[i] + data.b[i]) / data.maxVal[i];
   int hue_sat = data.hue[i] * data.saturation[i] / data.minVal[i];
 
-  data.finalVal[i] = grayscale*hue_sat; 
+  data.finalVal[i] = grayscale * hue_sat;
 }
 
-void complicatedCalculation()
-{
+void complicatedCalculation() {
 
   Coefficients_SOA d_x;
 
-  cudaMalloc(&d_x.r, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.g, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.b, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.hue, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.saturation, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.maxVal, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.minVal, IMG_SIZE*sizeof(int)); 
-  cudaMalloc(&d_x.finalVal, IMG_SIZE*sizeof(int)); 
+  cudaMalloc(&d_x.r, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.g, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.b, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.hue, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.saturation, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.maxVal, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.minVal, IMG_SIZE * sizeof(int));
+  cudaMalloc(&d_x.finalVal, IMG_SIZE * sizeof(int));
 
-  int num_blocks = IMG_SIZE/NUM_THREADS;
+  int num_blocks = IMG_SIZE / NUM_THREADS;
 
-  complicatedCalculation<<<num_blocks,NUM_THREADS>>>(d_x);
-  
+  complicatedCalculation<<<num_blocks, NUM_THREADS>>>(d_x);
+
   cudaFree(d_x.r);
   cudaFree(d_x.g);
   cudaFree(d_x.b);
@@ -60,17 +55,8 @@ void complicatedCalculation()
   cudaFree(d_x.finalVal);
 }
 
+int main(int argc, char *argv[]) {
 
-
-int main(int argc, char*argv[])
-{
-
-	complicatedCalculation();
-	return 0;
+  complicatedCalculation();
+  return 0;
 }
-
-
-
-
-
-
